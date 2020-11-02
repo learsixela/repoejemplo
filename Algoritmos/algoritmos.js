525e79822e61e60587e664a1cd74f8652dc194cd
israelpalma <ipalm001@gmail.com>
2020-11-02 18:53:37 -0300

    primeros ejercicios

--- Algoritmos/algoritmos.js
+++ Algoritmos/algoritmos.js
@@ -1,2 +1,36 @@
-mi script
-Prueba
+/** 1 **/
+/* las funciones nunca se ejecutan solas, solamente cuando son llamadas */
+function a() {
+    console.log('hello');
+}
+console.log('Dojo');
+//rspt:'Dojo',
+
+/** 2 las funciones pueden o no retornar valor*/
+function a() {
+    console.log('hello');
+    return 15;
+}
+//definicion de variable
+x = a(); //x = 15
+console.log('x is', x); // 'x is 15'
+//rspt:'hello','x is 15'
+
+/** */
+function a(n) { //n=3
+    console.log('n is', n);
+    return n + 15; //18
+}
+x = a(3); //x = 18
+console.log('x is', x);
+
+//rspt:'n is 3','x is 18'
+
+/** */
+function a(n) {
+    console.log('n is', n);
+    y = n * 2;
+    return y;
+}
+x = a(3) + a(5);
+console.log('x is', x);
