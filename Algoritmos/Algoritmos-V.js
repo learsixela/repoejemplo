b2f8f8f6003ea5e0cda6992867a9a2be354ed041
israelpalma <ipalm001@gmail.com>
2020-11-06 20:11:43 -0300

    Algoritmos 5.3

--- Algoritmos/Algoritmos-V.js
+++ Algoritmos/Algoritmos-V.js
@@ -17,6 +17,41 @@ function resetNegativos(arreglo) {
 var resultado = resetNegativos([1, 2, -1, -3]);
 console.log(resultado);
 
-/*Dado un array, mueve todos los valores un espacio de derecha a izquierda eliminando el primer valor y dejando un 0 para el último valor. Por ejemplo, moverAdelante([1,2,3]) debiera dar como resultado [2,3,0].*/
-/*Configura un array para que el resultado sean los valores en el orden contrario. Por ejemplo, returnReverso([1,2,3]) debe dar [3,2,1].*/
+/* 2 .- Dado un array, mueve todos los valores un espacio de derecha a izquierda 
+eliminando el primer valor 
+y dejando un 0 para el último valor. 
+Por ejemplo, moverAdelante([1,2,3]) debiera dar como resultado [2,3,0].
+[2,4,6,8] -> [4,6,8,0]
+*/
+
+function moverAdelante(arreglo) { //arreglo.length =  3 ; arreglo[arreglo.length-1] = 0
+    let nuevoArreglo = []; //[]
+    for (let i = 1; i < arreglo.length; i++) { //i=1
+        //nuevoArreglo[i-1]=arreglo[i];
+        nuevoArreglo.push(arreglo[i])
+    }
+    nuevoArreglo.push(0); //[2,3,0]
+    return nuevoArreglo;
+}
+
+console.log(moverAdelante([2, 4, 6, 8]));
+
+function moverAdelante2(arreglo) { //arreglo = [2,4,6,8]; //arreglo.length = 4 ; arreglo[arreglo.length-1] = 0
+    for (let i = 1; i < arreglo.length; i++) { //i=1>2>3
+        arreglo[i - 1] = arreglo[i];
+    }
+    //SIEMPRE la ultima posicion de un arrelo es x[x.length-1]
+    arreglo[arreglo.length - 1] = 0; //arreglo[4-2]
+    return arreglo;
+}
+console.log(moverAdelante2([2, 4, 6, 8])); //0,1,2,3;
+
+
+/*Configura un array para que el resultado sean los valores en el orden contrario. 
+Por ejemplo, returnReverso([1,2,3]) debe dar [3,2,1].*/
+
+
+
+
+
 /*Crea una función que cambie un array repitiendo sus valores originales (manteniendo el mismo orden). Por ejemplo, repetirValores([4,”Ulysses”, 42, false]) debiera dar [4,4, “Ulysses”, “Ulysses”, 42, 42, false, false].*/
