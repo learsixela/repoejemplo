cf42cc868d6eef77012f63f5582703dbe36852f5
israelpalma <ipalm001@gmail.com>
2020-11-06 18:58:18 -0300

    Algoritmos 5

---
+++ Algoritmos/Algoritmos-V.js
@@ -0,0 +1,5 @@
+/*Configura un array para que los valores negativos se transformen en 0. 
+Por ejemplo, resetNegativos([1,2,-1,-3]) debiera dar como resultado [1,2,0,0].*/
+/*Dado un array, mueve todos los valores un espacio de derecha a izquierda eliminando el primer valor y dejando un 0 para el último valor. Por ejemplo, moverAdelante([1,2,3]) debiera dar como resultado [2,3,0].*/
+/*Configura un array para que el resultado sean los valores en el orden contrario. Por ejemplo, returnReverso([1,2,3]) debe dar [3,2,1].*/
+/*Crea una función que cambie un array repitiendo sus valores originales (manteniendo el mismo orden). Por ejemplo, repetirValores([4,”Ulysses”, 42, false]) debiera dar [4,4, “Ulysses”, “Ulysses”, 42, 42, false, false].*/
