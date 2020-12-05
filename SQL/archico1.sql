6d61e5acedec7b82be17c0dc0590425fad834570
israelpalma <ipalm001@gmail.com>
2020-12-04 23:05:19 -0300

    script mysql 3

--- SQL/archico1.sql
+++ SQL/archico1.sql
@@ -30,9 +30,47 @@ SHOW TABLES;
 -- eliminar una tabla 
 DROP TABLE JUGETES;
 
+-- INSERCION DE REGISTROS
+-- INSERT INTO nombre_tabla (columna1, columna2 ...)
+-- VALUES (VALOR1, VALOR2);
 
+INSERT INTO personas (id, nombre,apellido) 
+VALUES (1,"Carlos", "Maldonado" );
 
+INSERT INTO personas (id, nombre,apellido) 
+VALUES (2,"Dani", "Ortega" ),
+        (3,"Juan", "Alvarez");
 
+-- PARA SABER LA ESTRUCTURA DE UNA TABLA
+-- DESCRIBE nombre_tabla;
+DESCRIBE personas;
 
+-- CONSULTAS A LAS TABLAS
+-- SELECT [] FROM nombre_tabla;
+SELECT * FROM personas;
 
+SELECT nombre FROM personas;
+SELECT nombre, id FROM personas;-- SELECT id, nombre FROM personas;
+SELECT id, apellido, nombre FROM personas;
+
+-- FILTROS
+SELECT * FROM personas
+WHERE id < 3
+;
+SELECT * FROM personas
+WHERE nombre = 'dani'
+and id= 3
+;
+
+-- LIKE
+-- '%%' o '%?' o '?%'
+
+SELECT * FROM personas
+WHERE nombre LIKE '%n%';
+
+SELECT * FROM personas
+WHERE nombre LIKE '%i';
+
+SELECT * FROM personas
+WHERE nombre LIKE 'C%';
 
